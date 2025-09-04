364c6be8e03b292a5d970fd4f883dee8447fcd30
Marinus <marinus.vonhof@live.nl>
2025-09-04 15:33:36 +0200

    Initial update

---
+++ config.js
@@ -0,0 +1,35 @@
+var respecConfig =
+{
+  //specStatus: "unofficial", // w3c-UD.css nodig, maar die is er niet
+  specStatus: "design",
+  //specType: "HR",             // Handreiking
+  //format: "markdown",         // Altijd "markdown"     
+  shortName: "Vacuumriolering", // Wordt gebruikt in de document URL
+  license: "cc0",
+   
+  editors: [{
+    name: "Eric Oosterom",
+    url: "mailto:eric.oosterom@rioned.org",
+    company: "Stichting RIONED",
+    companyURL: "https://www.riool.net"
+  }],
+  authors: [{
+    name: "Johan Post",
+    url: "mailto:johan.post@urbanwater.nl",
+    company: "Partners4UrbanWater",
+    companyURL: "https://www.urbanwater.nl/"
+  }],
+  otherLinks: [{
+    key: "GitHub", href: "https://github.com/stichtingrioned/Vacuumriolering/"
+  }],
+  //github: "https://github.com/StichtingRIONED/GWSW"
+  //-- Repositorynaam/issues op GitHub ------------------------------------------------
+  //issueBase: "https://github.com/StichtingRIONED/GWSW/issues/",
+
+  thisVersion: "https://stichtingrioned.github.io/Vacuumriolering/",
+  latestVersion: "", // gepubliceerde versie
+  edDraftURI: "https://stichtingrioned.github.io/Vacuumriolering/", // werkversie
+
+  maxTocLevel: 3, // Aantal niveau's ToC, default is 0/geen limiet
+  level2: true, // mv: hoofdstuk-nummering alleen voor de subsections (20210212)
+};
---
+++ main.md
@@ -0,0 +1,36 @@
+# Vacuümriolering
+
+<style>
+  .symbolSmall{width:20px;height:20px;margin-right:1em;vertical-align:middle}
+  .symbol{width:30px;height:30px;margin-right:1em;vertical-align:middle}
+</style>
+
+Stichting RIONED is initiatiefnemer en eigenaar van dit GitHub-project, Eric Oosterom is de verantwoordelijk projectmanager. 
+
+Vragen over deze website en het GWSW kunt u stellen via gwsw@rioned.org. 
+
+# Inleiding
+
+Context, GWSW-project
+
+# Beoogde toepassing
+
+Applicaties (extern en/of op GWSW Server)  
+Gegevensbehoefte
+
+# Inrichting van het deelmodel
+
+Belangrijkste GWSW-aanpassingen  
+Modelleerprincipes
+
+# Realisatie
+
+Deelnemers  
+Taakverdeling, rol werkgroep, projectleider, RIONED  
+Fasering in de tijd
+
+# Handleiding, toepassen
+
+Help bij  
+* opbouw datasets
+* gebruik apps
